f2b33986d98a80431ddee02e6c0ba78c8ab42606
Kevin Warui <waruimacharia@gmail.com>
2023-03-15 17:58:49 +0300

    Removed unused code

--- small_lisp.c
+++ small_lisp.c
@@ -9,21 +9,18 @@ static char input[2048];
 int main(int argc, char** argv)
 {
     /* Print version and Exit information*/
-    puts("small_lisp version 1.0\n");
-    puts("Press Ctrl+c to exit\n");
+    puts("small_lisp version 1.0");
+    puts("Press Ctrl+c to exit");
 
     while (1)
     {
-        char *input = readline ("> ");       
-
-        /* Output prompt and get input from user*/
-        //char* el_gets(*e, *count);
+        char *input = readline ("small_lisp> ");
 
         /* add input to history */
         //add_history(input);
 
         /* Output test string */
-        printf("Hey, it's working. This is your input: %s", input);
+        printf("Hey, it's working. This is your input: %s\n", input);
 
         /* free the memory */
         free(input);
